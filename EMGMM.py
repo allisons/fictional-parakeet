0829b7d53ac9535c4ba199791dffd9438738130d
allisons <allison.sliter@gmail.com>
2016-07-22 10:23:02 -0700

    Updated with numpy scipy math

--- EMGMM.py
+++ EMGMM.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # 
-# Copyright (c) 2012 Kyle Gorman
+# Copyright (c) 2016 Kyle Gorman, Allison Sliter
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@
 # 
 # em2gaus.py: Expectation maximization for mixture of two univariate Gaussians
 # Kyle Gorman <kgorman@ling.upenn.edu>
+# Updated with numpy functions by Allison Sliter allison.sliter@gmail.com
 # 
 # This code depends on numpy
 # 
@@ -37,54 +38,27 @@
 # course notes for COGS 502 at the University of Pennsylvania, taught by Mark 
 # Liberman and Stephen Isard. This particular code was written for my 2012 
 # University of Pennsylvania dissertation, though wasn't ultimately used.
-#
+# 
 # THIS IS EXPERIMENTAL CODE: PLEASE USE AT YOUR OWN RISK.
 
-from random import uniform
-from math import sqrt, log, exp, pi
+from __future__ import division
+import numpy as np
+from scipy.stats import norm, chi2
 
-from stats import chisqprob
-# requires:
-# 
-# http://www.nmr.mgh.harvard.edu/Neural_Systems_Group/gary/python/stats.py
-# http://www.nmr.mgh.harvard.edu/Neural_Systems_Group/gary/python/pstat.py
 
 ## default parameters
 _mix = .5
 _mu_min = 0.
-_mu_max = 1.
+_mu_max = 2.
 _sigma_min = .1
 _sigma_max = 1.
 ## number of random restarts
 _n_iterations = 10
 _rand_restarts = 1000
 
-class Gaussian(object):
-    """ 
-    Class representing a single univariate Gaussian
-    """ 
-
-    def __init__(self, mu, sigma):
-        self.mu = mu
-        self.sigma = sigma
-
-    def __repr__(self):
-        return 'Gaussian({0:4.6}, {1:4.6})'.format(self.mu, self.sigma)
-
-    def pdf(self, datum):
-        """
-        Returns the probability of a datum given the current parameters
-        
-        Write this in pure Python or scipy later
-        """
-        u = (datum - self.mu) / abs(self.sigma)
-        y = (1 / (sqrt(2 * pi) * abs(self.sigma))) * exp(-u * u / 2)
-        return y
-
-
 class GaussianMixture(object):
     """
-    Class representing mixture of two univariate Gaussians and their EM 
+    Class representing mixture of N univariate Gaussians and their EM 
     estimation
     
     data:      iterable of numerical values
@@ -92,92 +66,108 @@ class GaussianMixture(object):
     mu_max:    maximum start value for mean
     sigma_min: minimum start value for sigma
     sigma_max: maximum start value for sigma
-    mix:       optional mixing parameter (default: .5)
     """
 
-    def __init__(self, data, mu_min, mu_max, sigma_min, sigma_max, mix=_mix):
+    def __init__(self, data, mu_min, mu_max, sigma_min, sigma_max, num_gauss=2):
         self.data = data
-        self.one = Gaussian(uniform(mu_min, mu_max), 
-                            uniform(sigma_min, sigma_max))
-        self.two = Gaussian(uniform(mu_min, mu_max), 
-                            uniform(sigma_min, sigma_max))
+        self.num_gauss = num_gauss
+        self.gausses = np.array([norm(loc=np.random.uniform(low=mu_min, high=mu_max),  scale=np.random.uniform(low=sigma_min, high=sigma_max)) for _ in xrange(self.num_gauss)])
+        self.mix = np.empty(num_gauss, dtype=float)
+        self.mix.fill(1/self.num_gauss)
+        self.weights = np.zeros((len(data), num_gauss), dtype=float)
+
+    def set_mix(self, mix):
+        assert isinstance(mix, ndarray)
+        assert mix.shape == self.gausses
         self.mix = mix
-
-    def Estep(self):
+    
+    def freshen_loglike(self):
         """
-        Perform an E(stimation)-step, freshening up self.loglike in the process
-        and yielding tuples of weights (for whatever purpose)
+        Updates weights and calculates the log-likelihood of total model
+        (For post training evaluation)
         """
+        N, n = self.weights.shape
+        for i in xrange(N):
+            for j in xrange(n):
+                self.weights[i, j] = self.gausses[j].pdf(data[i])*self.mix[j]
+        self.loglike = np.sum(np.log(np.sum(self.weights, axis=1)))
+    
+    
+    def EMstep(self):
+        """
+        One full EM cycle, 
+        E(stimation step) - update the weight matrix given the data and the current gaussian parameters (and calculate loglike).
+        M(aximation step) - given weight matrix and data, update the parameters of the gaussians and the mix.
+        """ 
         # compute weights
-        self.loglike = 0. # = log(p = 1)
-        for datum in self.data:
-            # unnormalized weights
-            wp1 = self.one.pdf(datum) * self.mix
-            wp2 = self.two.pdf(datum) * (1. - self.mix)
-            # compute denominator
-            den = wp1 + wp2
-            # normalize
-            wp1 /= den
-            wp2 /= den
-            # add into loglike
-            self.loglike += log(wp1 + wp2)
-            # yield weight tuple
-            yield (wp1, wp2)
-
-    def Mstep(self, weights):
-        """
-        Perform an M(aximization)-step
-        """
-        # compute denominators
-        (left, rigt) = zip(*weights)
-        one_den = sum(left)
-        two_den = sum(rigt)
-        # compute new means
-        self.one.mu = sum(w * d / one_den for (w, d) in zip(left, data))
-        self.two.mu = sum(w * d / two_den for (w, d) in zip(rigt, data))
-        # compute new sigmas
-        self.one.sigma = sqrt(sum(w * ((d - self.one.mu) ** 2) \
-                                      for (w, d) in zip(left, data)) / one_den)
-        self.two.sigma = sqrt(sum(w * ((d - self.two.mu) ** 2) \
-                                      for (w, d) in zip(rigt, data)) / two_den)
-        # compute new mix
-        self.mix = one_den / len(data)
-
+        N, n = self.weights.shape
+        for i in xrange(N):
+            for j in xrange(n):
+                self.weights[i, j] = self.gausses[j].pdf(data[i])*self.mix[j]
+        #calculate loglikelihood of model
+        self.loglike = np.sum(np.log(np.sum(self.weights, axis=1)))
+        # normalize weights
+        for i in xrange(N):
+            self.weights[i] /= np.sum(self.weights[i])
+        
+        # compute next best gaussian parameters    
+        for i in xrange(self.num_gauss):
+            mean = np.sum((self.weights[:,i]*self.data)/np.sum(self.weights[:,i]))
+            std = np.sqrt(np.sum(self.weights[:,i]*((self.data-mean)**2))/np.sum(self.weights[:,i]))
+            self.gausses[i] = norm(loc=mean, scale=std)
+            self.mix[i] = np.sum(self.weights[:,i])
+        #normalize mix
+        self.mix /= np.sum(self.mix)
+    
     def iterate(self, N=1, verbose=False):
         """
         Perform N iterations, then compute log-likelihood
         """
         for i in xrange(1, N + 1):
-            self.Mstep(self.Estep())
+            self.EMstep()
             if verbose:
                 print '{0:2} {1}'.format(i, self)
-        self.Estep() # to freshen up self.loglike
+        
 
     def __repr__(self):
-        return 'GaussianMixture({0}, {1}, mix={2.03})'.format(self.one, 
-                                                        self.two, self.mix)
+        string = "gaussianMixture\nMean\tStd\tWeight\n"
+        
+        for i in xrange(self.num_gauss):
+            m, v = self.gausses[i].stats(moments='mv')
+            mix = self.mix[i]
+            string += "{0:4.6}\t{1:4.6}\t{2:4.6}\n".format(str(m), str(v), str(mix))
+        return string
+            
 
     def __str__(self):
-        return 'Mixture: {0}, {1}, mix={2:.03})'.format(self.one, 
-                                                        self.two, self.mix)
+        string = "gaussianMixture\nMean\tStd\tWeight\n"
+        
+        for i in xrange(self.num_gauss):
+            m, v = self.gausses[i].stats(moments='mv')
+            mix = self.mix[i]
+            string += "{0:4.6}\t{1:4.6}\t{2:4.6}\n".format(str(m), str(v), str(mix))
+        return string
 
 
 if __name__ == '__main__':
 
-    from numpy import mean, std
     from sys import argv, stderr
 
-    if len(argv) != 2: 
-        exit('USAGE: ./em2gaus.py DATA')
+    if len(argv) < 3: 
+        exit('USAGE: ./em2gaus.py DATA [NUM_GAUSSIANS]')
 
     ## read in data
     data = [float(d) for d in open(argv[1], 'r')]
+    if len(argv)>2:
+        num_gaussians = int(argv[2])
+    else:
+        num_gaussians = 2
 
     ## compute unimodal model
-    uni = Gaussian(mean(data), std(data))
-    uni_loglike = sum(log(uni.pdf(d)) for d in data)
+    uni = norm(loc=np.mean(data),  scale=np.std(data))
+    uni_loglike = np.sum(np.log(uni.pdf(d)) for d in data)
 
-    print 'Best singleton: {0}'.format(uni)
+    print 'Best singleton: {0:4.6}, {1:4.6}'.format(np.mean(data), np.std(data))
     print 'Null LL: {0:4.6}'.format(uni_loglike)
 
     ## find best one
@@ -186,7 +176,7 @@ if __name__ == '__main__':
     best_loglike = float('-inf')
     stderr.write('Computing best model with random restarts...\n')
     for i in xrange(_rand_restarts):
-        mix = GaussianMixture(data, _mu_min, _mu_max, _sigma_min, _sigma_max)
+        mix = GaussianMixture(data, _mu_min, _mu_max, _sigma_min, _sigma_max, num_gauss=num_gaussians)
         # I catch division errors from bad starts, and just throw them out...
         for i in xrange(_n_iterations):
             try:
@@ -200,4 +190,4 @@ if __name__ == '__main__':
     print 'Alternative LL: {0:4.6}'.format(best_gaus.loglike)
     test_stat = -2 * uni_loglike + 2 * best_gaus.loglike
     print 'Test statistic for LLR (Chi-sq, df=3): {0:4.6}'.format(test_stat)
-    print 'P = {0:4.6}'.format(chisqprob(test_stat, 3))
+    print 'P = {0:4.6}'.format(chi2.pdf(test_stat, df=3))
